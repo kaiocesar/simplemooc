acbdfd48ab3ed8683a22f70864349286f1dfd3f1
Kaio Cesar <tecnico.kaio@gmail.com>
2016-05-28 01:59:45 -0300

    core urls

--- simplemooc/urls.py
+++ simplemooc/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from simplemooc.core.views import *
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),	
-    url(r'^contact/$', 'simplemooc.core.views.contact', name='contact'),
-    url(r'^$', 'simplemooc.core.views.home', name='home'),
+    url(r'^', include('simplemooc.core.urls', namespace='core')),
 ]
